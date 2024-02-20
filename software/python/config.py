e76c62545dd163608e9324c327b5a005cb01d1b3
Pete Douma <pete.douma@gmail.com>
2024-02-20 14:51:33 -0500

    add the config

---
+++ software/python/config.py
@@ -0,0 +1,4 @@
+host = 'a3dogva4o0cdwc-ats.iot.us-east-1.amazonaws.com'
+cacert = "/AmazonRootCA1.pem"
+clientcert = "/c27625855e40b6eb67021faa474d064c4f0160a280c8891152e1fba8dcd22251-certificate.pem.crt"
+clientkey = "/c27625855e40b6eb67021faa474d064c4f0160a280c8891152e1fba8dcd22251-private.pem.key"
